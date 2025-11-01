c1446aa5b1e1003e78dc05952f3a9a53d7f8e3b0
Dariusz Filipiak <darekf77@gmail.com>
2025-11-01 15:50:35 +0100

    chore: taon errors app

--- request-failure/src/app.ts
+++ request-failure/src/app.ts
@@ -9,7 +9,13 @@ import Aura from '@primeng/themes/aura'; // @browser
 import { MaterialCssVarsModule } from 'angular-material-css-vars'; // @browser
 import { providePrimeNG } from 'primeng/config'; // @browser
 import { Observable, map } from 'rxjs';
-import { Taon, BaseContext, TAON_CONTEXT, EndpointContext } from 'taon/src';
+import {
+  Taon,
+  BaseContext,
+  TAON_CONTEXT,
+  EndpointContext,
+  TaonHttpResponseError,
+} from 'taon/src';
 import { UtilsOs } from 'tnp-core/src';
 
 import { HOST_CONFIG } from './app.hosts';
@@ -31,7 +37,13 @@ console.log('Your frontend host ' + HOST_CONFIG['MainContext'].frontendHost);
     <ul>
       <li *ngFor="let user of users$ | async">{{ user | json }}</li>
     </ul>
-    hello world from backend: <strong>{{ hello$ | async }}</strong> `,
+    hello world from backend: <strong>{{ hello$ | async }}</strong>
+
+    <button (click)="shortError()">shortError</button>
+    <button (click)="stackError()">stackError</button>
+    <button (click)="customCodeError()">customCodeError</button>
+    <button (click)="taonError()">taonError</button>
+    <button (click)="failInRow()">failInRow</button> `,
   styles: [
     `
       body {
@@ -50,6 +62,50 @@ export class RequestFailureComponent {
     .helloWorld()
     .request()
     .observable.pipe(map(r => r.body.text));
+
+  async shortError() {
+    // this.userApiService.userController.waitForFail();
+    try {
+      await this.userApiService.userController.helloWorld('short').request();
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async stackError() {
+    // this.userApiService.userController.waitForFail();
+    try {
+      await this.userApiService.userController.helloWorld('stack').request();
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async customCodeError() {
+    // this.userApiService.userController.waitForFail();
+    try {
+      await this.userApiService.userController
+        .helloWorld('customCode')
+        .request();
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async taonError() {
+    // this.userApiService.userController.waitForFail();
+    try {
+      await this.userApiService.userController
+        .helloWorld('taonError')
+        .request();
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async failInRow() {
+    this.userApiService.userController.waitForFail();
+  }
 }
 //#endregion
 //#endregion
@@ -121,10 +177,63 @@ class UserController extends Taon.Base.CrudController<User> {
   // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
   entityClassResolveFn = () => User;
 
+  count = 0;
+
+  async waitForFail(): Promise<void> {
+    await this._waitForProperStatusChange<string>({
+      actionName: 'helloWorld',
+      poolingInterval: 500,
+      request: ({ reqIndexNum }) => {
+        console.log(`Making request #${reqIndexNum}`);
+
+        if (reqIndexNum > 5) {
+          return this.helloWorld('customCode').request();
+        }
+        if (reqIndexNum > 4) {
+          return this.helloWorld('stack').request();
+        }
+        if (reqIndexNum > 3) {
+          return this.helloWorld('short').request();
+        }
+
+        return this.helloWorld().request();
+      },
+      statusCheck: response => response.body.text === 'hello world niga',
+      actionOnBackendError: ({ errorData, reqIndexNum }) => {
+        if (errorData instanceof TaonHttpResponseError) {
+          console.warn(errorData.body.json);
+        }
+        console.log({ errorData, reqIndexNum });
+        return reqIndexNum < 6;
+      },
+    });
+  }
+
   @Taon.Http.GET()
-  helloWorld(): Taon.Response<string> {
+  helloWorld(
+    @Taon.Http.Param.Query('errorType')
+    errorType?: 'short' | 'stack' | 'customCode' | 'taonError',
+  ): Taon.Response<string> {
     //#region @websqlFunc
-    return async (req, res) => 'hello world';
+    return async (req, res) => {
+      if (errorType === 'short') {
+        throw 'short error message here';
+      }
+      if (errorType === 'stack') {
+        throw new Error('message with stack trace here');
+      }
+      if (errorType === 'customCode') {
+        res.status(444).json({ customErrorMessage: 'Helo my friend' });
+        return;
+      }
+      if (errorType === 'taonError') {
+        Taon.error({
+          message: 'This is custom Taon error',
+          status: 499,
+        });
+      }
+      return `hello world`;
+    };
     //#endregion
   }
 
@@ -199,15 +308,6 @@ async function start(startParams?: Taon.StartParams): Promise<void> {
   //#region @backend
   console.log(`Hello in NodeJs backend! os=${os.platform()}`);
   //#endregion
-
-  if (UtilsOs.isBrowser) {
-    const users = (
-      await MainContext.getClassInstance(UserController).getAll().request()
-    ).body?.json;
-    for (const user of users || []) {
-      console.log(`user: ${user.name} - ${user.getHello()}`);
-    }
-  }
 }
 
 export default start;
