6f88be906e4ead80a70f6d78c1ae6cbe93eb05ed
Dariusz Filipiak <darekf77@gmail.com>
2025-11-01 11:55:46 +0100

    chore: request failure app

---
+++ request-failure/README.md
@@ -0,0 +1,5 @@
+# request-failure
+
+Hello from Standalone Project
+
+       
---
+++ request-failure/env.ts
@@ -0,0 +1,9 @@
+import type { EnvOptions } from 'tnp/src';
+
+const env: Partial<EnvOptions> = {
+  website: {
+    domain: 'request-failure.example.domain.com',
+    title: 'Request Failure',
+  },
+};
+export default env;
