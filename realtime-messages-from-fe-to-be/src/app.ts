269905a8389cd212509cbd07546625d12e1dcdb5
Dariusz Filipiak <darekf77@gmail.com>
2026-02-28 23:15:31 +0100

    refactor: realtime-messages-from-fe-to-be

--- realtime-messages-from-fe-to-be/src/app.ts
+++ realtime-messages-from-fe-to-be/src/app.ts
@@ -1,76 +1,440 @@
 //#region imports
-import { CommonModule } from '@angular/common'; // @browser
-import { Component, NgModule } from '@angular/core'; // @browser
-import { Taon, BaseContext } from 'taon/src';
+import * as os from 'os'; // @backend
+
+import { AsyncPipe, CommonModule, JsonPipe, NgFor } from '@angular/common'; // @browser
+import {
+  inject,
+  Injectable,
+  APP_INITIALIZER,
+  ApplicationConfig,
+  provideBrowserGlobalErrorListeners,
+  isDevMode,
+  mergeApplicationConfig,
+  provideZonelessChangeDetection,
+  signal,
+} from '@angular/core'; // @browser
+import { Component } from '@angular/core'; // @browser
+import { VERSION, OnInit } from '@angular/core'; // @browser
+import { toSignal } from '@angular/core/rxjs-interop'; // @browser
+import { MatButtonModule } from '@angular/material/button'; // @browser
+import { MatCardModule } from '@angular/material/card'; // @browser
+import { MatDividerModule } from '@angular/material/divider'; // @browser
+import { MatIconModule } from '@angular/material/icon'; // @browser
+import { MatListModule } from '@angular/material/list'; // @browser
+import { MatTabsModule } from '@angular/material/tabs'; // @browser
+import {
+  provideClientHydration,
+  withEventReplay,
+} from '@angular/platform-browser';
+import {
+  provideRouter,
+  Router,
+  RouterLinkActive,
+  RouterModule,
+  RouterOutlet,
+  ActivatedRoute,
+  Routes,
+  Route,
+  withHashLocation,
+  withComponentInputBinding,
+} from '@angular/router';
+import { provideServiceWorker } from '@angular/service-worker';
+import { provideServerRendering, withRoutes } from '@angular/ssr';
+import { RenderMode, ServerRoute } from '@angular/ssr';
+import Aura from '@primeng/themes/aura'; // @browser
+import { providePrimeNG } from 'primeng/config'; // @browser
+import { BehaviorSubject, Observable, map, switchMap } from 'rxjs';
+import {
+  Taon,
+  TaonBaseContext,
+  TAON_CONTEXT,
+  EndpointContext,
+  TaonBaseAngularService,
+  TaonEntity,
+  StringColumn,
+  TaonBaseAbstractEntity,
+  TaonBaseCrudController,
+  TaonController,
+  GET,
+  TaonMigration,
+  TaonBaseMigration,
+  TaonContext,
+} from 'taon/src';
+import { Utils, UtilsOs } from 'tnp-core/src';
 
 import { HOST_CONFIG } from './app.hosts';
+import { ENV_ANGULAR_NODE_APP_BUILD_PWA_DISABLE_SERVICE_WORKER } from './lib/env/env.angular-node-app';
+// @placeholder-for-imports
 //#endregion
 
 //#region constants
 const eventsKey = 'eventsKey';
+
+const firstHostConfig = (Object.values(HOST_CONFIG) || [])[0];
+console.log('Your backend host ' + firstHostConfig?.host);
+console.log('Your frontend host ' + firstHostConfig?.frontendHost);
 //#endregion
 
-//#region realtime-subscribers component
+//#region realtime-messages-from-fe-to-be component
+
 //#region @browser
 @Component({
-  standalone: false,
+  selector: 'app-root',
+
+  imports: [
+    // RouterOutlet,
+    AsyncPipe,
+    MatCardModule,
+    MatIconModule,
+    MatDividerModule,
+    MatButtonModule,
+    MatListModule,
+    MatTabsModule,
+    RouterModule,
+    JsonPipe,
+    CommonModule,
+  ],
+  // // Uncomment to have simples template
+  // template: `
+  //   @if (itemsLoaded()) {
+  //     <router-outlet />
+  //   }
+  // `,
   template: `push global event to backend with click of a button<br />
     <br />
     <button (click)="notifyBackend()">notify backend</button> `,
 })
-export class RealtimeMessagesFromFeToBeComponent {
+export class RealtimeMessagesFromFeToBeApp implements OnInit {
   counter = 0;
+
   notifyBackend() {
     console.log('notifyBackend');
-    MainContext.__refSync.realtimeClient.triggerCustomEvent(
+    RealtimeMessagesFromFeToBeContext.realtime.client.triggerCustomEvent(
       eventsKey,
       `hello ${++this.counter} message from frontend`,
     );
   }
+
+  itemsLoaded = signal(false);
+
+  navItems =
+    RealtimeMessagesFromFeToBeClientRoutes.length <= 1
+      ? []
+      : RealtimeMessagesFromFeToBeClientRoutes.filter(
+          r => r.path !== undefined,
+        ).map(r => ({
+          path: r.path === '' ? '/' : `/${r.path}`,
+          label: r.path === '' ? 'Home' : `${r.path}`,
+        }));
+
+  activatedRoute = inject(ActivatedRoute);
+
+  get activePath(): string {
+    return globalThis?.location.pathname?.split('?')[0];
+  }
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    console.log(globalThis?.location.pathname);
+    // TODO set below from 1000 to zero in production
+    Taon.removeLoader(1000).then(() => {
+      this.itemsLoaded.set(true);
+    });
+  }
+
+  taonMode = UtilsOs.isRunningInWebSQL() ? 'websql' : 'normal nodejs';
+
+  angularVersion = VERSION.full;
+
+  userApiService = inject(UserApiService);
+
+  router = inject(Router);
+
+  private refresh = new BehaviorSubject<void>(undefined);
+
+  readonly users = toSignal(
+    this.refresh.pipe(
+      switchMap(() =>
+        this.userApiService.userController
+          .getAll()
+          .request()
+          .observable.pipe(map(r => r.body.json)),
+      ),
+    ),
+    { initialValue: [] },
+  );
+
+  readonly hello$ = this.userApiService.userController
+    .helloWorld()
+    .request()
+    .observable.pipe(map(r => r.body.text));
+
+  async deleteUser(userToDelete: User): Promise<void> {
+    await this.userApiService.userController
+      .deleteById(userToDelete.id)
+      .request();
+    this.refresh.next();
+  }
+
+  async addUser(): Promise<void> {
+    const newUser = new User();
+    newUser.name = `user-${Math.floor(Math.random() * 1000)}`;
+    await this.userApiService.userController.save(newUser).request();
+    this.refresh.next();
+  }
+
+  forceShowBaseRootApp = false;
+
+  navigateTo(item: { path: string; label: string }): void {
+    if (item.path === '/') {
+      if (this.forceShowBaseRootApp) {
+        return;
+      }
+      this.forceShowBaseRootApp = true;
+      return;
+    }
+    this.forceShowBaseRootApp = false;
+    this.router.navigateByUrl(item.path);
+  }
 }
+//#endregion
+
+//#endregion
+
+//#region  realtime-messages-from-fe-to-be api service
+
+//#region @browser
+@Injectable({
+  providedIn: 'root',
+})
+export class UserApiService extends TaonBaseAngularService {
+  userController = this.injectController(UserController);
+
+  getAll(): Observable<User[]> {
+    return this.userController
+      .getAll()
+      .request()
+      .observable.pipe(map(r => r.body.json));
+  }
+}
+//#endregion
+
+//#endregion
+
+//#region  realtime-messages-from-fe-to-be routes
+//#region @browser
+export const RealtimeMessagesFromFeToBeServerRoutes: ServerRoute[] = [
+  {
+    path: '**',
+    renderMode: RenderMode.Prerender,
+  },
+];
+export const RealtimeMessagesFromFeToBeClientRoutes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: () => {
+      if (RealtimeMessagesFromFeToBeClientRoutes.length === 1) {
+        return '';
+      }
+      return RealtimeMessagesFromFeToBeClientRoutes.find(r => r.path !== '')!
+        .path!;
+    },
+  },
+  // PUT ALL ROUTES HERE
+  // @placeholder-for-routes
+];
 //#endregion
 //#endregion
 
-//#region  realtime-subscribers context
-const MainContext = Taon.createContext(() => ({
-  ...HOST_CONFIG['MainContext'],
-  useIpcWhenElectron: true,
-  contextName: 'MainContext',
-  contexts: { BaseContext },
-  controllers: {},
-  entities: {},
-  database: true,
-  logs: {
-    realtime: true,
+//#region  realtime-messages-from-fe-to-be app configs
+//#region @browser
+export const RealtimeMessagesFromFeToBeAppConfig: ApplicationConfig = {
+  providers: [
+    provideZonelessChangeDetection(),
+    {
+      provide: TAON_CONTEXT,
+      useFactory: () => RealtimeMessagesFromFeToBeContext,
+    },
+    providePrimeNG({
+      theme: {
+        preset: Aura,
+      },
+    }),
+    {
+      provide: APP_INITIALIZER,
+      multi: true,
+      useFactory: () => RealtimeMessagesFromFeToBeStartFunction,
+    },
+    provideBrowserGlobalErrorListeners(),
+    // remove withHashLocation() to use SSR
+    provideRouter(
+      RealtimeMessagesFromFeToBeClientRoutes,
+      withHashLocation(),
+      withComponentInputBinding(),
+    ),
+    provideClientHydration(withEventReplay()),
+    provideServiceWorker('ngsw-worker.js', {
+      enabled:
+        !isDevMode() && !ENV_ANGULAR_NODE_APP_BUILD_PWA_DISABLE_SERVICE_WORKER,
+      registrationStrategy: 'registerWhenStable:30000',
+    }),
+  ],
+};
+
+export const RealtimeMessagesFromFeToBeServerConfig: ApplicationConfig = {
+  providers: [
+    provideServerRendering(withRoutes(RealtimeMessagesFromFeToBeServerRoutes)),
+  ],
+};
+
+export const RealtimeMessagesFromFeToBeConfig = mergeApplicationConfig(
+  RealtimeMessagesFromFeToBeAppConfig,
+  RealtimeMessagesFromFeToBeServerConfig,
+);
+//#endregion
+//#endregion
+
+//#region  realtime-messages-from-fe-to-be entity
+@TaonEntity({ className: 'User' })
+class User extends TaonBaseAbstractEntity {
+  //#region @websql
+  @StringColumn()
+  //#endregion
+  name?: string;
+
+  getHello(): string {
+    return `hello ${this.name}`;
+  }
+}
+//#endregion
+
+//#region  realtime-messages-from-fe-to-be controller
+@TaonController({ className: 'UserController' })
+class UserController extends TaonBaseCrudController<User> {
+  // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+  entityClassResolveFn = () => User;
+
+  @GET()
+  helloWorld(): Taon.Response<string> {
+    //#region @websqlFunc
+    return async (req, res) => 'hello world';
+    //#endregion
+  }
+
+  @GET()
+  getOsPlatform(): Taon.Response<string> {
+    //#region @websqlFunc
+    return async (req, res) => {
+      //#region @backend
+      return os.platform(); // for normal nodejs backend return real value
+      //#endregion
+
+      return 'no-platform-inside-browser-and-websql-mode';
+    };
+    //#endregion
+  }
+}
+//#endregion
+
+//#region  realtime-messages-from-fe-to-be migration
+
+//#region @websql
+@TaonMigration({
+  className: 'UserMigration',
+})
+class UserMigration extends TaonBaseMigration {
+  userController = this.injectRepo(User);
+
+  async up(): Promise<any> {
+    const superAdmin = new User();
+    superAdmin.name = 'super-admin';
+    await this.userController.save(superAdmin);
+  }
+}
+//#endregion
+
+//#endregion
+
+//#region  realtime-messages-from-fe-to-be context
+var RealtimeMessagesFromFeToBeContext = Taon.createContext(() => ({
+  ...HOST_CONFIG['RealtimeMessagesFromFeToBeContext'],
+  contexts: { TaonBaseContext },
+
+  //#region @websql
+  /**
+   * In production use specyfic for this context name
+   * generated migration object from  ./migrations/index.ts.
+   */
+  migrations: {
+    UserMigration,
   },
+  //#endregion
+
+  controllers: {
+    UserController,
+  },
+  entities: {
+    User,
+  },
+  database: true,
+  // disabledRealtime: true,
 }));
 //#endregion
 
-//#region start
-async function start() {
-  await MainContext.initialize();
+//#region  realtime-messages-from-fe-to-be start function
+export const RealtimeMessagesFromFeToBeStartFunction = async (
+  startParams?: Taon.StartParams,
+): Promise<void> => {
+  await RealtimeMessagesFromFeToBeContext.initialize();
 
   //#region @websql
   console.log(`Server subscribed to events ${eventsKey}`);
-  MainContext.__refSync.realtimeServer
+  RealtimeMessagesFromFeToBeContext.realtime.server
     .listenChangesCustomEvent(eventsKey)
     .subscribe(data => {
       // TODO @LAST fix this for electron
       console.log('data from frontend:', data);
     });
   //#endregion
-}
 
-export default start;
+  //#region initialize auto generated active contexts
+  const autoGeneratedActiveContextsForApp: TaonContext[] = [
+    // @placeholder-for-contexts-init
+  ];
+
+  const priorityContexts = [
+    // put here manual priority for contexts if needed
+  ];
+
+  const activeContextsForApp: TaonContext[] = [
+    ...priorityContexts,
+    ...autoGeneratedActiveContextsForApp.filter(
+      c => !priorityContexts.includes(c),
+    ),
+  ];
+
+  for (const activeContext of activeContextsForApp) {
+    await activeContext.initialize();
+  }
+  //#endregion
+
+  //#region @backend
+  if (
+    startParams?.onlyMigrationRun ||
+    startParams?.onlyMigrationRevertToTimestamp
+  ) {
+    process.exit(0);
+  }
+  //#endregion
+
+  //#region @backend
+  console.log(`Hello in NodeJs backend! os=${os.platform()}`);
+  //#endregion
+};
 //#endregion
 
-//#region  realtime-messages-from-fe-to-be module
-//#region @browser
-@NgModule({
-  declarations: [RealtimeMessagesFromFeToBeComponent],
-  imports: [CommonModule],
-  exports: [RealtimeMessagesFromFeToBeComponent],
-})
-export class RealtimeMessagesFromFeToBeModule {}
-//#endregion
+//#region default export
+export default RealtimeMessagesFromFeToBeStartFunction;
 //#endregion
